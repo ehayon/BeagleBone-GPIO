4efdf4b427b7e3b85c4be0af4ef38fc144636ee6
Ethan Hayon <ehayon@gmail.com>
2012-08-25 12:34:27 -0400

    tests for digitalRead and digitalWrite

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 
 int main() {
 	while(1) {
+		/* write a fancy led pattern
 		digitalWrite(USR3, HIGH);
 		usleep(50000);
 		digitalWrite(USR2, HIGH);
@@ -18,6 +19,15 @@ int main() {
 		usleep(50000);
 		digitalWrite(USR0, LOW);
 		usleep(50000);
+		*/
+
+	  	// set P8_3 as an INPUT with PULLDOWN enabled
+	  	pinMode(P8_3, INPUT, 7, PULLDOWN);
+		// turn on USR2 LED when P8_3 is set high
+		digitalWrite(USR2, digitalRead(P8_3));
+
+		// delay 10ms
+	  	usleep(10000);
 	}	
 	//munmap(map, fd);	
 	return 0;
