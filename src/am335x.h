06b64c1717efa3044a62d7f13b533d2b5a6e8e4f
Ethan Hayon <ehayon@gmail.com>
2012-08-28 22:18:04 -0400

    update gpio registers for USR LEDS

--- src/am335x.h
+++ src/am335x.h
@@ -13,47 +13,87 @@
 #ifndef _AM335X_H_
 #define _AM335X_H_
 
-#define MMAP_OFFSET (0x44E0F000)
+#define MMAP_OFFSET (0x44C00000)
 #define MMAP_SIZE   (0x481AEFFF-MMAP_OFFSET)
 
+/* Clock Module Memory Registers */
+#define CM_WKUP (0x44E00400)
+#define CM_WKUP_ADC_TSC_CLKCTRL (CM_WKUP+0xBC)
+#define CM_WKUP_MODULEMODE_ENABLE (0x02)
+#define CM_WKUP_IDLEST_DISABLED (0x03<<16)
+
+#define CM_PER (0x44E00000)
+#define CM_PER_EPWMSS1_CLKCTRL (CM_PER+0xCC)
+#define CM_PER_EPWMSS0_CLKCTRL (CM_PER+0xD4)
+#define CM_PER_EPWMSS2_CLKCTRL (CM_PER+0xD8)
+
+
+/* GPIO Memory Registers */
 #define GPIO_REGISTER_SIZE (4)
 
-#define GPIO0 		(0x44E07000)
+#define GPIO0 	(0x44E07000)
 #define GPIO1		(0x4804C000)
 #define GPIO2		(0x481AC000)
 #define GPIO3		(0x481AE000)
 
 #define GPIO_CLEARDATAOUT (0x190)
 #define GPIO_SETDATAOUT   (0x194)
-#define GPIO_OE			  (0x134)
+#define GPIO_OE			      (0x134)
 #define GPIO_DATAOUT      (0x13C)
 #define GPIO_DATAIN       (0x138)
 
+/* Analog Digital Converter Memory Registers */
+#define ADC_TSC (0x44E0D000)
+
+#define ADC_CTRL (ADC_TSC+0x40)
+#define ADC_STEPCONFIG_WRITE_PROTECT_OFF (0x01<<2)
+#define ADC_STEPENABLE (ADC_TSC+0x54)
+
+#define ADCSTEPCONFIG1 (ADC_TSC+0x64)
+#define ADCSTEPDELAY1  (ADC_TSC+0x68)
+#define ADCSTEPCONFIG2 (ADC_TSC+0x6C)
+#define ADCSTEPDELAY2  (ADC_TSC+0x70)
+#define ADCSTEPCONFIG3 (ADC_TSC+0x74)
+#define ADCSTEPDELAY3  (ADC_TSC+0x78)
+#define ADCSTEPCONFIG4 (ADC_TSC+0x7C)
+#define ADCSTEPDELAY4  (ADC_TSC+0x80)
+#define ADCSTEPCONFIG5 (ADC_TSC+0x84)
+#define ADCSTEPDELAY5  (ADC_TSC+0x88)
+#define ADCSTEPCONFIG6 (ADC_TSC+0x8C)
+#define ADCSTEPDELAY6  (ADC_TSC+0x90)
+#define ADCSTEPCONFIG7 (ADC_TSC+0x94)
+#define ADCSTEPDELAY7  (ADC_TSC+0x98)
+#define ADCSTEPCONFIG8 (ADC_TSC+0x9C)
+#define ADCSTEPDELAY8  (ADC_TSC+0xA0)
+
+#define ADC_FIFO0DATA (ADC_TSC+0x100)
+#define ADC_FIFO_MASK (0xFFF)
+
 typedef struct s_PWM {
-	char muxmode; /*!< mux mode, 0-7, see am335x technical manual */
-	char *name;   /*!< name of pwm pin, i.e.: "EHRPWM2B" */
-	char *path;   /*!< path to the pwm, i.e.: "ehrpwm.2:1" */
+  char muxmode; /*!< mux mode, 0-7, see am335x technical manual */
+  char *name;   /*!< name of pwm pin, i.e.: "EHRPWM2B" */
+  char *path;   /*!< path to the pwm, i.e.: "ehrpwm.2:1" */
 } PWM;
 
 typedef struct s_PIN {
-	char *name;   /*!< readable name of pin, i.e.: "GPIO1_21", see beaglebone user guide */
-	unsigned int gpio_bank; /*!< which of the four gpio banks is this pin in, i.e.: GPIO1, r 0x4804C000 */
-	uint8_t gpio; /*!< pin number on the am335x processor */
-	uint8_t bank_id; /*!< pin number within each bank, should be 0-31 */
-	char *mux;    /*!< file name for setting mux */
-	uint8_t eeprom; /*!< position in eeprom */
-	unsigned char pwm_present; /*!< whether or not this pin can be used for PWM */
-	PWM pwm;      /*!< pwm struct if pwm_present is true */
+  char *name;   /*!< readable name of pin, i.e.: "GPIO1_21", see beaglebone user guide */
+  unsigned int gpio_bank; /*!< which of the four gpio banks is this pin in, i.e.: GPIO1, r 0x4804C000 */
+  uint8_t gpio; /*!< pin number on the am335x processor */
+  uint8_t bank_id; /*!< pin number within each bank, should be 0-31 */
+  char *mux;    /*!< file name for setting mux */
+  uint8_t eeprom; /*!< position in eeprom */
+  unsigned char pwm_present; /*!< whether or not this pin can be used for PWM */
+  PWM pwm;      /*!< pwm struct if pwm_present is true */
 
 } PIN;
 
 #define TRUE 1
 #define FALSE 0
 
-#define USR0  ((PIN){	"GPIO1_21",	   GPIO1,     0,   21,   "",				0,     FALSE, 0   })
-#define USR1  ((PIN){	"GPIO1_22",	   GPIO1,     0,   22,   "",				0,     FALSE, 0   })
-#define USR2  ((PIN){	"GPIO1_23",	   GPIO1,     0,   23,   "",				0,     FALSE, 0   })
-#define USR3  ((PIN){	"GPIO1_24",	   GPIO1,     0,   24,   "",				0,     FALSE, 0   })
+#define USR0  ((PIN){	  "GPIO1_21",	   GPIO1,     0,   21,   "",				0,     FALSE, 0   })
+#define USR1  ((PIN){	  "GPIO1_22",	   GPIO1,     0,   22,   "",				0,     FALSE, 0   })
+#define USR2  ((PIN){	  "GPIO1_23",	   GPIO1,     0,   23,   "",				0,     FALSE, 0   })
+#define USR3  ((PIN){	  "GPIO1_24",	   GPIO1,     0,   24,   "",				0,     FALSE, 0   })
 #define P8_3  ((PIN){   "GPIO1_6",	   GPIO1,     38, 	6,	 "gpmc_ad6",        26,    FALSE, 0   })
 #define P8_4  ((PIN){   "GPIO1_7",     GPIO1,     39, 	7,	 "gpmc_ad7",        27,    FALSE, 0   })
 #define P8_5  ((PIN){   "GPIO1_2",     GPIO1,     34, 	2,	 "gpmc_ad2",        22,    FALSE, 0   })
